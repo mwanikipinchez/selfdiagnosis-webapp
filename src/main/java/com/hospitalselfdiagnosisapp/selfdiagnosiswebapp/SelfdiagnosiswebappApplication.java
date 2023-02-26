9b5fe28a4021562b05c7bed8a35df50499d9b8da
Peter Mwaniki <mwanikipinchez@gmail.com>
2023-02-27 00:06:40 +0300

    Initial commit

---
+++ src/main/java/com/hospitalselfdiagnosisapp/selfdiagnosiswebapp/SelfdiagnosiswebappApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalselfdiagnosisapp.selfdiagnosiswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfdiagnosiswebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfdiagnosiswebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitalselfdiagnosisapp/selfdiagnosiswebapp/SelfdiagnosiswebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalselfdiagnosisapp.selfdiagnosiswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfdiagnosiswebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
