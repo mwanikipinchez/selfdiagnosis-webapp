791e5d8c8f9d7fbe5af1fcdf283819039bb2e4f6
Peter Mwaniki <mwanikipinchez@gmail.com>
2023-02-27 17:32:54 +0300

    thymeleaf configuration

--- src/main/java/com/hospitalselfdiagnosisapp/selfdiagnosiswebapp/controller/PatientController.java
+++ src/main/java/com/hospitalselfdiagnosisapp/selfdiagnosiswebapp/controller/PatientController.java
@@ -4,6 +4,7 @@ package com.hospitalselfdiagnosisapp.selfdiagnosiswebapp.controller;
 import com.hospitalselfdiagnosisapp.selfdiagnosiswebapp.model.Patient;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class PatientController {
     public String registrationForm(Model model) {
         model.addAttribute("patient", new Patient());
         return "signup";
+    }
+
+    @PostMapping("/register")
+    public String save(Patient patient){
+
+        return "redirect:/login";
     }
 
     @GetMapping("/login")
